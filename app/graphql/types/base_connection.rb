a124885cdf50eefd6bbb10c8c753d2bc404efb57
AMBE Hideki <ykc@daycrift.net>
2023-11-19 17:44:02 +0900

    feat: add total_count to base_connection

--- app/graphql/types/base_connection.rb
+++ app/graphql/types/base_connection.rb
@@ -4,5 +4,12 @@ module Types
   class BaseConnection < Types::BaseObject
     # add `nodes` and `pageInfo` fields, as well as `edge_type(...)` and `node_nullable(...)` overrides
     include GraphQL::Types::Relay::ConnectionBehaviors
+
+    # see: https://graphql-ruby.org/type_definitions/extensions#customizing-connections
+    field :total_count, Integer, null: false
+
+    def total_count
+      object.items&.count || 0
+    end
   end
 end
