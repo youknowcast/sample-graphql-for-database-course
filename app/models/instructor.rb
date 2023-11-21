487aedf371256fceda375bc9fde3cd6ba673e3ca
AMBE Hideki <ykc@daycrift.net>
2023-11-21 16:24:57 +0900

    fix: invalid relation

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -4,7 +4,7 @@ class Instructor < ActiveRecord::Base
   self.table_name = 'instructor'
 
   belongs_to :department, foreign_key: :dept_name
-  has_many :instructor_students, foreign_key: :i_id, class_name: 'Advisor'
+  has_many :instructor_students, foreign_key: :i_ID, class_name: 'Advisor'
   has_many :students, through: :instructor_students
   has_many :teaches, foreign_key: :ID
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -6,6 +6,6 @@ class Student < ActiveRecord::Base
   belongs_to :department, foreign_key: :dept_name
   has_many :takes, foreign_key: :id
   # Advisor は一人のみ
-  has_one :student_instructor, foreign_key: :s_id, class_name: 'Advisor'
+  has_one :student_instructor, foreign_key: :s_ID, class_name: 'Advisor'
   has_one :instructor, through: :student_instructor
 end
